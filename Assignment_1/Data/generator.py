9d3d8e4ff71c18e3590fe92fea0e98098877d7e9
Kwesi <kwecobi@gmail.com>
2023-09-11 20:11:58 -0400

    Student version data complete

--- Assignment_1/Data/generator.py
+++ Assignment_1/Data/generator.py
@@ -37,10 +37,29 @@ def generate_data(num_samples=100, dimension=1, test_size=0.2, m=7, b=3, low=0, 
     xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size)
 
     return xtrain, xtest, ytrain, ytest
+
+
+
+def generate_sine_data(num_samples=100, dimension=1, test_size=0.2, amplitude=1, frequency=1, phase=0, low=0, high=10):
+    x = np.random.uniform(low=low, high=high, size=(num_samples, dimension))
+
+    if dimension == 1:
+        y = amplitude * np.sin(frequency * x + phase)
+    else:
+        y = np.zeros((num_samples, 1))
+        for d in range(dimension):
+            y += amplitude * np.sin(frequency * x[:, d].reshape(-1, 1) + phase)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size)
+
+    return np.array(x_train), np.array(x_test), np.array(y_train), np.array(y_test)
+
+
+
 # Generate data
 # xtrain, xtest, ytrain, ytest = generate_data()
 
-def q1():
+def q1_a():
     xtrain, xtest, ytrain, ytest = generate_data(num_samples=10000,test_size=0.2)
     xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
     print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
@@ -49,39 +68,33 @@ def q1():
         "test": (xtest, ytest)
     }
 
-
-def q2():
-    xtrain, xtest, ytrain, ytest = generate_data(num_samples=10000, dimension=2, test_size=0.2, low=-10, high=10)
-    xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
-    print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
-    print(xtrain)
-    print(ytrain)
-    # num_samples = 1000
-    # x = np.random.uniform(-10, 10, size=(num_samples, 2))
-    # noise = np.random.normal(0, 1, num_samples)
-    # y = 3 * x[:, 0] + 4 * x[:, 1] + 5 + noise
-    # print(x.shape, y.shape)
-    # # pass
-
-def q2b():
-    xtrain, xtest, ytrain, ytest = generate_data(num_samples=10000, dimension=3, test_size=0.2, low=0, high=10)
+def q1_b():
+    xtrain, xtest, ytrain, ytest = generate_data(num_samples=10000, dimension=4, test_size=0.2, low=0, high=10)
     xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
     print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
     print(xtrain)
     print(ytrain)
 
-def q3():
-    pass
-
-def q4():
-    pass
-
-def q5():
-    pass
-
-def q6():
-    pass
 
 
+def q2_a():
+    xtrain, xtest, ytrain, ytest = generate_sine_data(num_samples=10000, dimension=1, test_size=0.2)
+    xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
+    print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
+    print(xtrain)
+    print(ytrain)
 
-q2b()
+
+def q2_b():
+    xtrain, xtest, ytrain, ytest = generate_sine_data(num_samples=10000, dimension=4, test_size=0.2, low=0, high=10)
+    xtrain, xtest, ytrain, ytest = np.array(xtrain, dtype=np.float32), np.array(xtest, dtype=np.float32), np.array(ytrain, dtype=np.float32), np.array(ytest, dtype=np.float32)
+    print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
+    print(xtrain)
+    print(ytrain)
+
+
+
+q1_a()
+q1_b()
+q2_a()
+q2_b()
--- Assignment_1/Questions/1b.py
+++ Assignment_1/Questions/1b.py
@@ -13,24 +13,4 @@ from Data.generator import q1
 from Model.evaluate.evaluate import evaluate_model
 
 
-
-Number_of_iterations = 10000
-Step_size = 0.5
-
-class Network(layers.BaseNetwork):
-    # TODO: you might need to pass additional arguments to init for prob 2, 3, 4 and mnist
-    def __init__(self, data_layer):
-        # you should always call __init__ first
-        super().__init__()
-        # TODO: define your network architecture here
-        self.linear = layers.Linear(data_layer, num_out_features=2)
-        self.bias = layers.Bias(self.linear)
-        # For prob 3 and 4:
-        # layers.ModuleList can be used to add arbitrary number of layers to the network
-        # e.g.:
-        # self.MY_MODULE_LIST = layers.ModuleList()
-        # for i in range(N):
-        #     self.MY_MODULE_LIST.append(layers.Linear(...))
-
-        # TODO: always call self.set_output_layer with the output layer of this network (usually the last layer)
-        self.set_output_layer(self.bias)
+# Please use the template file to answer the question
